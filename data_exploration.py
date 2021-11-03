53e34db833988b9d739bea6eb73b1afaf402fa9f
aznur <zceenur@ucl.ac.uk>
2021-11-03 17:28:36 +0000

    update

--- data_exploration.py
+++ data_exploration.py
@@ -1,1 +1,15 @@
-# Write code that explores your data set
+# Pandas and matplotlib libraries will be used for data exploration.
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    # The data set contains information about public transport journeys by type of transport.
+    # The prepared CSV file (from data_preparation.py) with the data will be used.
+
+    # Reading CSV file with the prepared data set and storing it in the dataframe.
+    df = pd.read_csv('prepared_dataset.csv')
+
+
+
+    print(df)
--- data_preparation.py
+++ data_preparation.py
@@ -1,1 +1,21 @@
-# Write code that prepares your data
+# Pandas library will be used for data preparation.
+import pandas as pd
+
+
+if __name__ == '__main__':
+    # The data set contains information about public transport journeys by type of transport.
+    # Both Excel and CSV files of the data set are saved in the "data" folder.
+
+    # Reading CSV file with the data set and storing it in the dataframe.
+    df = pd.read_csv('data/tfl-journeys-type.csv')
+
+    # Deleting all the rows with null (missing) values.
+    df = df.dropna()
+
+    # Remove a single column with the name 'Reporting Period'.
+    df = df.drop(['Reporting Period'], axis=1)
+
+    print(df)
+
+    # Save the dataframe to a new CSV file. Row names will not be saved.
+    df.to_csv("prepared_dataset.csv", index=False)
